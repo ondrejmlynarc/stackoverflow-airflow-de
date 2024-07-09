c42a08e731f41e03f9c93944b8f20832858327ca
Ondrej <84532874+ondrejmlynarc@users.noreply.github.com>
2024-07-09 04:40:39 +0200

    Update stackof_dag.py

---
+++ stackof_dag.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+from stackof_etl import run_stackof_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 2, 2),
+    'email': ['ondrejmlynarc@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'stackof_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl = PythonOperator(
+    task_id='complete_stackof_etl',
+    python_callable=run_stackof_etl,
+    dag=dag, 
+)
+
+run_etl
